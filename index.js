7ec9795f5e8b51bc79acd8b78afda80217b16b5e
tjn0994 <quangtin12b5@gmail.com>
2018-07-24 23:40:57 +0700

    Chat app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 // import App from './AppSaga';
 // import {App} from './AppTabNavigator';
 // import {App} from './AppDrawerNavigator';
-import App from './AppReduxForm';
+// import App from './AppReduxForm';
+import App from './AppChat';
 
 AppRegistry.registerComponent('foody', () => App);
